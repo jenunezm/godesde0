c49e43e2d895f64d9a4b2603f0589a7e1fd5254a
Javier Enrique <jenunez@yahoo.com>
2023-06-22 14:13:19 -0500

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jenunezm/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jenunezm/godesde0/variables"
+)
+
+func main() {
+	fmt.Println("Hola Mundo del GO")
+
+	variables.MuestroEnteros()
+}
